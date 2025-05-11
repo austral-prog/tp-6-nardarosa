573d285c1ed8fae397f46a6115d4657595a4eb01
nardarosa <nardarosa2050@gmail.com>
2025-05-11 18:35:52 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -14,15 +14,11 @@ def remove_elements(list_to_remove_elements):
         return list_to_remove_elements
     else:
         return []
-list_to_remove_elements = input("Enter a list of elements separated by spaces: ").split()
-print(remove_elements(list_to_remove_elements))
 
 def add_elements(list_to_add_elements):
     list_to_add_elements.append("Yellow")
     list_to_add_elements.insert(0, "Pink")
     return list_to_add_elements
-list_to_add_elements = input("Enter a list of elements separated by spaces: ").split()
-print(add_elements(list_to_add_elements))   
 
 def is_empty(list_to_check):
     if len(list_to_check) == 0:
@@ -31,8 +27,6 @@ def is_empty(list_to_check):
     else:
         print(False)
         return False
-list_to_check = input("Enter a list of elements separated by spaces: ").split()
-is_empty(list_to_check)
 
 def check_lists(list_to_compare1, list_to_compare2):
     if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
@@ -42,9 +36,6 @@ def check_lists(list_to_compare1, list_to_compare2):
             return False
     elif len(list_to_compare1) < 2 or len(list_to_compare2) < 2:
         return False 
-list_to_compare1 = input("Enter the first list of elements separated by spaces: ").split()
-list_to_compare2 = input("Enter the second list of elements separated by spaces: ").split()
-check_lists(list_to_compare1, list_to_compare2)
 
 def list_of_lists(list_of_lists_to_modify): 
     if len(list_of_lists_to_modify) != 3:
@@ -53,8 +44,3 @@ def list_of_lists(list_of_lists_to_modify):
     list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
     list_of_lists_to_modify[2] = list_of_lists_to_modify[2][-2:]
     return list_of_lists_to_modify
-list_of_lists1 = input("Enter the first list of lists separated by spaces: ").split()
-list_of_lists2 = input("Enter the second list of lists separated by spaces: ").split()
-list_of_lists3 = input("Enter the third list of lists separated by spaces: ").split()
-list_of_lists_to_modify = [list_of_lists1, list_of_lists2, list_of_lists3]
-print(list_of_lists(list_of_lists_to_modify))
